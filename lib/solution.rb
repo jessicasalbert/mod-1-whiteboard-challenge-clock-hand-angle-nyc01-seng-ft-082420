6941987a37e06e64c2f14e4d7e4bb5bb188a1b84
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-25 01:35:09 +0000

    Automatically backed up by Learn

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,19 @@
 def clock_angle(time)
-    # code your solution here
+  if time.length == 5
+    minutes = (time[3] + time[4]).to_i 
+    minuteDegrees = minutes * 6
+    hourDegrees = ((time[0] + time[1]).to_i * 30) + minutes * 0.5
+  end
+  if time.length == 4
+    minutes = (time[2] + time[3]).to_i 
+    minuteDegrees = minutes * 6
+    hourDegrees = (time[0].to_i * 30) + minutes * 0.5
+  end
+
+  if hourDegrees > minuteDegrees
+    return hourDegrees - minuteDegrees
+  else
+    return 360 - (minuteDegrees - hourDegrees)
+  end
+
 end
